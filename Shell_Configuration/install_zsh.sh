6828fec0679edc50ecb19226e2e8e27a76033bab
Zheyuan Mei <55944499+chushengbajinban@users.noreply.github.com>
2025-04-22 17:22:49 +0800

    Update install_zsh.sh

--- Shell_Configuration/install_zsh.sh
+++ Shell_Configuration/install_zsh.sh
@@ -1,30 +1,24 @@
 #!/bin/bash
 
-# 更新软件包列表
+# update
 echo "apt-get update"
 sudo apt-get update
 
-# 安装必要软件
 sudo apt-get install -y zsh git
 
-# 非交互式安装oh-my-zsh
+# install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-# 更改默认shell为zsh
+# set default shell to zsh
 chsh -s $(which zsh)
 
-# 安装Powerlevel10k主题
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-# 安装插件
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-# 添加插件到配置
 sed -i 's/plugins=(git)/plugins=(git zsh-syntax-highlighting zsh-autosuggestions)/g' ~/.zshrc
 
-# 配置.zshrc文件
 sed -i 's/ZSH_THEME="[^"]*"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' ~/.zshrc
 
-# 重新加载配置并切换到新zsh环境
 exec zsh
